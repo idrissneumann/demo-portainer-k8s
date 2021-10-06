d06a2399d00dd93dadc119edb148562a63e2ae08
Idriss Neumann <neumann.idriss@gmail.com>
2021-10-06 14:17:13 +0000

    Update mirror.sh

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,5 +5,5 @@ REPO_PATH="/home/centos/demo-portainer-k8s/"
 cd "${REPO_PATH}" && git pull origin main || :
 git push github main 
 git push pgitlab main
-git push pgitlab internal
+git push internal main 
 exit 0
